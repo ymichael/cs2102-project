b6b56de9f71f0361e3c0fe24ec00970466fc39b5
Michael Yong <wrong92@gmail.com>
2014-02-19 13:54:52 +0800

    Fix stuff.

--- db.py
+++ db.py
@@ -2,28 +2,29 @@ import sqlite3
 
 db = sqlite3.connect(":memory:")
 
-queries_run_once = ["""\
-    CREATE TABLE users (
-        name varchar(255)
+queries_run_once = [
+    """\
+    CREATE TABLE test (
+        val varchar(255)
     )""",
-    "INSERT INTO users (name) VALUES ('Hello')"
+    "INSERT INTO test (val) VALUES ('Hello')"
 ]
 
-test_query = "SELECT * FROM users"
+test_query = "SELECT * FROM test"
 
-
-def users():
+def init():
     cursor = db.cursor()
-
     for q in queries_run_once:
         cursor.execute(q)
         db.commit()
 
+def test():
+    cursor = db.cursor()
     cursor.execute(test_query)
     return cursor.fetchall()
 
-
 def main():
+    init()
     print users()
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import db
 from flask import Flask
 app = Flask(__name__)
 
+db.init()
 
 @app.route("/")
 def hello():
-    users = db.users()
-    return "%s world!" % users[0]
+    test = db.test()
+    return "%s world!" % test[0]
 
 if __name__ == "__main__":
     app.run()
