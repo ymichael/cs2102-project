56bf62fe88a0d8e1dea714c640bc553da743e529
Michael Yong <wrong92@gmail.com>
2014-02-23 15:54:06 +0800

    Change schema.py
    - Remove test schema
    - Make user.name NOT NULL

--- db/schema.py
+++ db/schema.py
@@ -3,16 +3,9 @@
 # 2. Run db.update_schema() in a python shell.
 SCHEMA = [
     (1, """\
-        CREATE TABLE test (
-             id int,
-             tmp text
-       )
-    """),
-    (2, "DROP TABLE test"),
-    (3, """\
         CREATE TABLE users (
             id integer PRIMARY KEY,
-            name varchar(255),
+            name varchar(255) NOT NULL,
             first_name varchar(50),
             last_name varchar(50),
             email varchar(255) NOT NULL,
