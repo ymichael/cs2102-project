3c6cb4ed87d3208c5aa37d5508400724889d93e5
= <wrong92@gmail.com>
2014-03-25 01:17:53 +0800

    Fix bug in fts.

--- db/fts.py
+++ db/fts.py
@@ -1,10 +1,21 @@
 import db
 
 def init():
-    populate_virtual_table()
+    maybe_populate_virtual_table()
 
 
-def populate_virtual_table():
+def has_created():
+    sql = """\
+        SELECT COUNT(*) AS count FROM listing_search"""
+    with db.DatabaseCursor() as cursor:
+        obj = cursor.execute(sql).fetchone()
+    return obj['count'] != 0
+
+
+def maybe_populate_virtual_table():
+    if has_created():
+        return
+
     # Populate fts table with existing data.
     sql = """\
         INSERT INTO listing_search(lid, content)
